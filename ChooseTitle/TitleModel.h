767136d2cc51aa399ed2144bc1694e0b31bf35e5
Superman <703353105@qq.com>
2018-07-15 12:43:31 +0800

    first commit

---
+++ ChooseTitle/TitleModel.h
@@ -0,0 +1,13 @@
+//
+//  TitleModel.h
+//  ChooseTitle
+//
+//  Created by Superman on 2018/7/14.
+//  Copyright © 2018年 Superman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TitleModel : NSObject
+
+@end
